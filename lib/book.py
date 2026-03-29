7d7c683055adfe940ab4f7e3892281b555305395
Charles <charlesngatia.cn@gmail.com>
2026-03-29 07:49:44 +0300

    .

--- lib/book.py
+++ lib/book.py
@@ -1,9 +1,17 @@
 class Book:
     def __init__(self, title, page_count):
         self.title = title
+        self._page_count = None
+        self.page_count = page_count
 
-        if isinstance(page_count, int):
-            self.page_count = page_count
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, value):
+        if isinstance(value, int):
+            self._page_count = value
         else:
             print("page_count must be an integer")
 
