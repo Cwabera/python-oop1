98d5d27ab41bd92e806d514121346c0b7834a68d
Charles <charlesngatia.cn@gmail.com>
2026-03-29 07:47:09 +0300

    ...

--- lib/book.py
+++ lib/book.py
@@ -2,10 +2,10 @@ class Book:
     def __init__(self, title, page_count):
         self.title = title
 
-        if not isinstance(page_count, int):
-            raise Exception("page_count must be an integer")
-
-        self.page_count = page_count
+        if isinstance(page_count, int):
+            self.page_count = page_count
+        else:
+            print("page_count must be an integer")
 
     def turn_page(self):
-        return "Flipped the page!"
+        print("Flipping the page...wow, you read fast!")
--- lib/coffee.py
+++ lib/coffee.py
@@ -1,13 +1,13 @@
 class Coffee:
-    def __init__(self, size, price):
+    def __init__(self, price, size):
+        self.price = price
+
         allowed_sizes = ["small", "medium", "large"]
 
-        if size not in allowed_sizes:
-            raise Exception("size must be small, medium, or large")
-
-        self.size = size
-        self.price = price
+        if size in allowed_sizes:
+            self.size = size
+        else:
+            print("size must be small, medium, or large")
 
     def tip(self, amount):
         self.price += amount
-        return self.price
